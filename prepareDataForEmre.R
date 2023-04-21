50bf220d47dd9539c660178ae91056a1acd6b390
Sophie Donnet <sophie.donnet@inrae.fr>
2023-04-21 15:07:11 +0200

    Preparing data Emre. Fin

--- prepareDataForEmre.R
+++ prepareDataForEmre.R
@@ -10,7 +10,7 @@ library(gtools)
 source('functions_PiedsArbres.R')
 #------------------------------------------------------------------- 
 
-########################### 
+###########################  Functions
 modifDataPiedArbres = function(dataPiedsArbres){
   dataPiedsArbres <- dataPiedsArbres %>% mutate(TB_taxa = as.factor(TB_taxa)) %>% mutate(soil = as.factor(TB_soil.grill))
   dataPiedsArbres <- dataPiedsArbres %>% mutate(STREET_number  =  TB_number..in.the.street.)
@@ -53,8 +53,6 @@ dataPiedsArbres_2014_2018 <- modifDataPiedArbres(read.delim(paste0("Tree_bases_"
 matSitePlant_2014_2018 <- buildMatrixSitePlants(dataPiedsArbres_2014_2018)
 
 #-------------------- 
-
-
 years <- "2009_2012"
 dataPiedsArbres_2009_2012 <- modifDataPiedArbres(read.delim(paste0("Tree_bases_",years,".txt")))
 matSitePlant_2009_2012 <- buildMatrixSitePlants(dataPiedsArbres_2009_2012)
@@ -69,16 +67,13 @@ matSitePlant_2009_2012 <- buildMatrixSitePlants(dataPiedsArbres_2009_2012)
 # ------------- en colonnes (plantes)
 list_plants_2009_2012 <- colnames(matSitePlant_2009_2012)
 list_plants_2014_2018 <- colnames(matSitePlant_2014_2018)
-
-
-
-list_all_plants <- sort(union(list_plants_2009_2012, list_plants_2014_2018))
+list_all_plants <- union(list_plants_2009_2012, list_plants_2014_2018)
 
 
 miss_plants_2009_2012 <- setdiff(list_all_plants, list_plants_2009_2012)
+length(miss_plants_2009_2012)
 matSitePlant_2009_2012_miss <- matrix(0,nrow(matSitePlant_2009_2012),length(miss_plants_2009_2012) )
 colnames(matSitePlant_2009_2012_miss) <- miss_plants_2009_2012
-
 matSitePlant_2009_2012_complete <-cbind(matSitePlant_2009_2012, matSitePlant_2009_2012_miss)
 matSitePlant_2009_2012_complete <- matSitePlant_2009_2012_complete[,order(colnames(matSitePlant_2009_2012_complete))]
 
@@ -86,24 +81,23 @@ matSitePlant_2009_2012_complete <- matSitePlant_2009_2012_complete[,order(colnam
 miss_plants_2014_2018 <- setdiff(list_all_plants, list_plants_2014_2018)
 matSitePlant_2014_2018_miss <- matrix(0,nrow(matSitePlant_2014_2018),length(miss_plants_2014_2018) )
 colnames(matSitePlant_2014_2018_miss) <- miss_plants_2014_2018
-
 matSitePlant_2014_2018_complete <-cbind(matSitePlant_2014_2018, matSitePlant_2014_2018_miss)
 matSitePlant_2014_2018_complete <- matSitePlant_2014_2018_complete[,order(colnames(matSitePlant_2014_2018_complete))]
 
-# ------------- en lignes  (sites)
+# ------------- en lignes  (sites) ! remove sites which are not observed on both period. 
 list_all_sites <- sort(union(rownames(matSitePlant_2009_2012_complete), rownames(matSitePlant_2014_2018_complete)))
 miss_sites_2009_2012 <- setdiff(list_all_sites, rownames(matSitePlant_2009_2012_complete))
 miss_sites_2014_2018 <- setdiff(list_all_sites, rownames(matSitePlant_2014_2018_complete))
+miss_all_sites <- union(miss_sites_2014_2018,miss_sites_2009_2012)
 
-matSitePlant_2014_2018_miss_2 <- matrix(NA,length(miss_sites_2014_2018),ncol(matSitePlant_2014_2018_complete))
-matSitePlant_2014_2018_complete2 <-rbind(matSitePlant_2014_2018_complete, matSitePlant_2014_2018_miss_2)
-rownames(matSitePlant_2014_2018_complete2) <- c(rownames(matSitePlant_2014_2018_complete),miss_sites_2014_2018)
-matSitePlant_2014_2018_complete2 <- matSitePlant_2014_2018_complete2[order(rownames(matSitePlant_2014_2018_complete2)),]
 
-matSitePlant_2009_2012_miss_2 <- matrix(NA,length(miss_sites_2009_2012),ncol(matSitePlant_2009_2012_complete))
-matSitePlant_2009_2012_complete2 <-rbind(matSitePlant_2009_2012_complete, matSitePlant_2009_2012_miss_2)
-rownames(matSitePlant_2009_2012_complete2) <- c(rownames(matSitePlant_2009_2012_complete),miss_sites_2009_2012)
-matSitePlant_2009_2012_complete2 <- matSitePlant_2009_2012_complete2[order(rownames(matSitePlant_2009_2012_complete2)),]
+W1 <- which(rownames(matSitePlant_2014_2018_complete) %in% miss_all_sites)
+matSitePlant_2014_2018_complete2 <-matSitePlant_2014_2018_complete[-W1, ]
+dim(matSitePlant_2014_2018_complete2)
+
+W2 <- which(rownames(matSitePlant_2009_2012_complete) %in% miss_all_sites)
+matSitePlant_2009_2012_complete2 <-matSitePlant_2009_2012_complete[-W2, ]
+dim(matSitePlant_2009_2012_complete2)
 
 
 ##########################################################################################
@@ -113,39 +107,62 @@ matSitePlant_2009_2012_complete2 <- matSitePlant_2009_2012_complete2[order(rowna
 # rue / grill ou pas / quel type d'arbres
 descriptSites_2009_2012 <- dataPiedsArbres_2009_2012 %>%group_by(site) %>% filter(row_number() == 1) %>% select(site,STREET,soil,TB_tree) %>% arrange(site)
 descriptSites_dummy_2009_2012 <- descriptSites_2009_2012  %>%  dummy_cols(select_columns = c("STREET","soil","TB_tree"))
-#descriptSites_dummy_2009_2012 <- descriptSites_dummy_2009_2012 %>% select(-c("STREET","soil" ,"TB_tree"))
 
-# rue / grill ou pas / quel type d'arbres
+ 
 descriptSites_2014_2018 <- dataPiedsArbres_2014_2018 %>%group_by(site) %>% filter(row_number() == 1) %>% select(site,STREET,soil,TB_tree) %>% arrange(site)
 descriptSites_dummy_2014_2018 <- descriptSites_2014_2018 %>%  dummy_cols(select_columns = c("STREET","soil","TB_tree"))
-#descriptSites_dummy_2014_2018 <- descriptSites_dummy_2014_2018 %>% select(-c("STREET", "soil" ,"TB_tree"))
 
 
 
-#-------------------- complete covar
+#-------------------- remove covar dfor site not observed both years
 
-w <- which(descriptSites_dummy_2014_2018$site %in% miss_sites_2009_2012)
-descriptSites_dummy_2014_2018[w,]
-descriptSites_dummy_2009_2012_complete <- rbind(descriptSites_dummy_2009_2012,descriptSites_dummy_2014_2018[w,])
-descriptSites_dummy_2009_2012_complete <- descriptSites_dummy_2009_2012_complete[order(descriptSites_dummy_2009_2012_complete$site), ]
-
-
-
-w <- which(descriptSites_dummy_2009_2012$site %in% miss_sites_2014_2018)
-descriptSites_dummy_2009_2012[w,]
-descriptSites_dummy_2014_2018_complete <- rbind(descriptSites_dummy_2014_2018,descriptSites_dummy_2009_2012[w,])
-descriptSites_dummy_2014_2018_complete <- descriptSites_dummy_2014_2018_complete[order(descriptSites_dummy_2014_2018_complete$site), ]
+w1 <- which(as.character(descriptSites_dummy_2014_2018$site) %in% miss_all_sites)
+descriptSites_dummy_2014_2018_complete <- descriptSites_dummy_2014_2018[-w1,]
+ 
+w2 <- which(as.character(descriptSites_dummy_2009_2012$site) %in% miss_all_sites)
+descriptSites_dummy_2009_2012_complete <- descriptSites_dummy_2009_2012[-w2,]
 
 nrow(descriptSites_dummy_2009_2012_complete)
 nrow(descriptSites_dummy_2014_2018_complete)
 
 
-descriptSites_dummy_2009_2012_complete$site[800:810]
-descriptSites_dummy_2014_2018_complete$site[800:810]
+u = sample(1:1310,1)
+descriptSites_dummy_2009_2012_complete$site[u:(u+10)]
+descriptSites_dummy_2014_2018_complete$site[u:(u+10)]
 u <- which(descriptSites_dummy_2009_2012_complete$TB_tree != descriptSites_dummy_2014_2018_complete$TB_tree)
+u <- which(descriptSites_dummy_2009_2012_complete$soil != descriptSites_dummy_2014_2018_complete$soil)
+length(u)
+
+
 descriptSites_dummy_2009_2012_complete[u,]
 descriptSites_dummy_2014_2018_complete[u,]
 
-as.character(descriptSites_dummy_2014_2018_complete$site)[descriptSites_dummy_2009_2012_complete$TB_tree != descriptSites_dummy_2014_2018_complete$TB_tree]
+
+###################################" Final save 
+matSitePlant_2009_2012 <- matSitePlant_2009_2012_complete2
+matSitePlant_2014_2018 <- matSitePlant_2014_2018_complete2
+descriptSites_dummy_2009_2012 <- descriptSites_dummy_2009_2012_complete   %>% select(-c("STREET", "soil" ,"TB_tree"))
+descriptSites_dummy_2014_2018 <- descriptSites_dummy_2014_2018_complete   %>% select(-c("STREET", "soil" ,"TB_tree"))
 
 
+################ Add position
+
+Coordonnees_PA <- read.csv("~/WORK_LOCAL/RECHERCHE/TRAVAUX_DE_RECHERCHE/Louvet-Appoline/SBMForUrbanEcology/dataAppoline/Coordonnees_PA.csv", sep=";")
+Coordonnees_PA <- Coordonnees_PA %>% mutate(site = as.factor(paste(PA_voie,PA_numérobis,sep="_")))
+Coordonnees_PA <- Coordonnees_PA[which(Coordonnees_PA$site %in% as.character(descriptSites_dummy_2009_2012$site)), ] 
+Coordonnees_PA <- Coordonnees_PA[order(as.character(Coordonnees_PA$site)), ]
+
+descriptSites_dummy_2009_2012 <- descriptSites_dummy_2009_2012 %>% mutate(COORD_X  = Coordonnees_PA$COORD_X)
+descriptSites_dummy_2009_2012 <- descriptSites_dummy_2009_2012 %>% mutate(COORD_Y  = Coordonnees_PA$COORD_Y)
+
+descriptSites_dummy_2014_2018<- descriptSites_dummy_2014_2018 %>% mutate(COORD_X  = Coordonnees_PA$COORD_X)
+descriptSites_dummy_2014_2018 <- descriptSites_dummy_2014_2018 %>% mutate(COORD_Y  = Coordonnees_PA$COORD_Y)
+
+
+save(matSitePlant_2014_2018 , descriptSites_dummy_2014_2018 , file = 'dataForEmre/dataPiedsArbres_2014_2018.Rdata')
+save(matSitePlant_2009_2012 , descriptSites_dummy_2009_2012 , file = 'dataForEmre/dataPiedsArbres_2009_2012.Rdata')
+
+dim(matSitePlant_2009_2012)
+dim(matSitePlant_2014_2018)
+dim(descriptSites_dummy_2014_2018)
+dim(descriptSites_dummy_2009_2012)
